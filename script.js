b5d2ae1e098ec6d20b57a27810bb6779877f8254
Ayush Dwivedi <dwivediayush028@gmail.com>
2025-02-25 13:56:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
-//complete this code
 class Person {
-	constructor(name, number){
-		this.name = name;
-		this.age = age;
-	}
+  constructor(name, age) {
+    this._name = name; 
+    this._age = age;
+  }
 
-	get name(){
-		return this.name;
-	}
-	set name(newName){
-		return this.name = newName
-	}
-	
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === "string" && newName.length > 0) {
+      this._name = newName;
+    } else {
+      console.error("Invalid name");
+    }
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(newAge) {
+    if (typeof newAge === "number" && newAge > 0) {
+      this._age = newAge;
+    } else {
+      console.error("Age must be a positive number");
+    }
+  }
 }
 
 class Student extends Person {
+  constructor(name, age) {
+    super(name, age); 
+  }
 
-	constructor(name, number){
-		super(name, number)
-	}
-	
-	study(){
-		console.log(`${this.name} is studying`)
-	}
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
 
 class Teacher extends Person {
-	constructor(name, number){
-		super(name, number)
-	}
-	study(){
-		console.log(`${this.name} is teaching`)
-	}
+  constructor(name, age) {
+    super(name, age); 
+  }
+
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
 
 // Do not change the code below this line
